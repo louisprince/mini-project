97b2306134c78e76ee35a6f540fc0079b537d292
Louis Prince <6587777+louisprince@users.noreply.github.com>
2026-05-15 14:06:39 +0100

    .

--- main.tf
+++ main.tf
@@ -10,3 +10,14 @@ terraform {
 provider "aws" {
   region = "eu-west-2"
 }
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.0.0/23" # https://mxtoolbox.com/subnetcalculator.aspx
+  availability_zone       = "eu-west-2a"
+  map_public_ip_on_launch = true
+}
